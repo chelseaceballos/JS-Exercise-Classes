de753acf430fe0ea836d0d2798ca00798779e1df
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-25 12:12:04 -0800

    pm tasks all complete

--- index.js
+++ index.js
@@ -88,7 +88,8 @@ class Airplane {
    }
    drive(distance){
      this.odometer = this.odometer + distance;
-     this.tank = this.milesPerGallon - distance; // I'M PRETTY SURE MY COMMON SENSE IS FAILING ME
+     this.tank = this.tank - this.milesPerGallon; // I'M PRETTY SURE MY COMMON SENSE IS FAILING ME
+ 
    }
 
   }
@@ -105,7 +106,7 @@ class Airplane {
 
       - Instances of Lambdasian should be able to `.speak()`:
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
-          + {name} and {location} of course come from the instance's own properties.
+          + {name} and {location} of course come from the instansce's own properties.
   */
  class Lambdasian {
     constructor(attrs){
@@ -117,6 +118,11 @@ class Airplane {
       return`Hello my name is ${this.name}, I am from ${this.location}`;
     }
   }
+  const personOne = new Lambdasian({
+    name:'Harris',
+    age: 2,
+    location:'California'
+  })
 
   /*
     TASK 4
@@ -147,6 +153,11 @@ class Airplane {
      return `${student.name} receives a perfect score on ${subject}`;
    }
  }
+ const personTwo = new Instructor({
+  name:'Gabby',
+  age: 27,
+  location:'NYC'
+})
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -175,13 +186,19 @@ class Airplane {
      listSubjects(){
        return `${this.favSubjects}`
      }
-     PRAssignment(){
-       return `${student.name} has submitted a PR for ${this.subject}`;
+     PRAssignment(subject){ //<--- recieves a subject as an argument
+       return `${this.student.name} has submitted a PR for ${subject}`;
      }
      sprintChallenge(){
        return `${student.name} has begun sprint challenge on ${this.subject}`;
      }
  }
+ const personThree = new Student({
+   name:'Chelsea',
+   age: 22,
+   location:'California',
+   subject: 'math'
+ })
   
   /*
     TASK 6
@@ -194,7 +211,8 @@ class Airplane {
       - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
 
       - ProjectManager instances have the following methods:
-          + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
+          + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`//PASSES
+
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
  class ProjectManager extends Instructor {
@@ -206,10 +224,15 @@ class Airplane {
      standUp(channel){
      return `${this.name} announces to ${channel}, @channel standy times!`
      }
-     debugsCode(studentObject){
+     debugsCode(student, subject){
        return `${this.name} debugs ${student.name}'s code on ${subject}`
      }
  }
+ const personFour = new ProjectManager({
+  name:'Britt',
+  age: 25,
+  location:'Toronto'
+})
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
