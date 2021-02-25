8dd7b651cca0897bb0746b3ec9bc461162cc4571
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-25 09:53:21 -0800

    added methods to pm

--- index.js
+++ index.js
@@ -192,6 +192,7 @@ class Airplane {
           + `favInstructor`: i.e. Sean
       - Its constructor calls the parent constructor passing to it what it needs.
       - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
+
       - ProjectManager instances have the following methods:
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
@@ -201,6 +202,12 @@ class Airplane {
        super(attrs)
        this.gradClassName = attrs.gradClassName;
        this.favInstructor = attrs.favInstructor;
+     }
+     standUp(slackChannel){
+     returns `${this.name} announces to ${channel}, @channel standy times!`
+     }
+     debugsCode(studentObject){
+       returns `${this.name} debugs ${student.name}'s code on ${subject}`
      }
  }
   /*
